c473fe3aca2ddada3bba461b920c45826b6c15fa
Manpreet Singh <mannyy91s@gmail.com>
2016-10-30 18:45:33 -0400

    added eventlisteners and  react components

--- js/global.js
+++ js/global.js
@@ -2,5 +2,37 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Todos from '../components/Todos'
 
-
 // Your code goes here
+//variables
+var inputText = document.querySelector('#inputNote')
+var inputBtn = document.querySelector('#addBtn')
+var inputValue
+inputText.addEventListener('keypress', enter)
+inputBtn.addEventListener('click', addToDo)
+
+//array
+var todos = []
+console.log(todos)
+
+//functions
+function enter(event) {
+    if ( event.key === 'Enter'){
+        addToDo()
+    }
+}
+function addToDo() {
+    var inputValue = document.querySelector('#inputNote').value
+    if(inputValue !== ''){
+        console.log(inputValue)
+        todos.push(inputValue)
+        renderView(todos)
+        document.querySelector('#inputNote').value = ''
+    }
+}
+
+window.renderView = (data) => {
+    ReactDOM.render(
+        <Todos data={data} />,
+        document.getElementById('inputResults')
+    )
+}
